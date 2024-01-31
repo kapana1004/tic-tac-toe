66c543f23c9ae8cb18d03b42360edf97891a6872
George Kapanadze <52297225+kapana1004@users.noreply.github.com>
2024-01-31 16:19:58 +0400

    add defineWinner function

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,12 @@ function App() {
   const [turn, setTurn] = useState(
     <img className="w-[16px] h-[16px]" src={smallX} alt="x" />
   );
+  let statusOfWinner;
 
   function handleClick(i) {
+    if (defineWinner(squares) || squares[i]) {
+      return;
+    }
     if (squares[i]) return;
     const nextSquares = squares.slice();
     if (xIsNext) {
@@ -26,6 +30,34 @@ function App() {
     }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
+
+    const winner = defineWinner(nextSquares);
+    console.log(winner);
+    if (winner) {
+      statusOfWinner = "Winner: " + winner;
+    }
+  }
+  function defineWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
   }
 
   return (
@@ -77,6 +109,7 @@ function App() {
           <span className=" text-[12px]"> O (P1)</span>
         </div>
       </div>
+      <div> {statusOfWinner ? <span> {statusOfWinner} </span> : null}</div>
     </div>
   );
 }
