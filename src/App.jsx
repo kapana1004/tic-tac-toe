dbfe20a81291765b8d44a79064fc046f0117dca8
George Kapanadze <52297225+kapana1004@users.noreply.github.com>
2024-02-03 00:22:24 +0400

    add winner recognition function

--- src/App.jsx
+++ src/App.jsx
@@ -12,31 +12,25 @@ function App() {
   const [turn, setTurn] = useState(
     <img className="w-[16px] h-[16px]" src={smallX} alt="x" />
   );
-  let statusOfWinner;
+  const [winner, setWinner] = useState(null);
 
-  function handleClick(i) {
-    if (defineWinner(squares) || squares[i]) {
+  const handleClick = (i) => {
+    if (winner || squares[i]) {
       return;
     }
-    if (squares[i]) return;
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = <img className=" pl-[30px]" src={xImg} alt="X" />;
 
-      setTurn(<img className="w-[16px] h-[16px]" src={smallZero} alt="0" />);
-    } else {
-      nextSquares[i] = <img className=" pl-[30px]" src={zeroImg} alt="0" />;
-      setTurn(<img className="w-[16px] h-[16px]" src={smallX} alt="x" />);
-    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
 
-    const winner = defineWinner(nextSquares);
-    console.log(winner);
-    if (winner) {
-      statusOfWinner = "Winner: " + winner;
+    const winnerFound = defineWinner(nextSquares);
+    if (winnerFound) {
+      setWinner(winnerFound);
     }
-  }
+  };
+
   function defineWinner(squares) {
     const lines = [
       [0, 1, 2],
@@ -48,6 +42,7 @@ function App() {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (
@@ -58,58 +53,54 @@ function App() {
         return squares[a];
       }
     }
+
+    return null;
   }
 
   return (
-    <div className=" flex flex-col  items-center min-h-[100vh] bg-[#1A2A33]">
-      <div className=" flex mt-[24px] mb-[64px] bg-[#1A2A33] w-[327px] h-[40px] justify-between">
-        <div className=" flex flex-row w-[72px] h-[32px] justify-between">
+    <div className="flex flex-col items-center min-h-[100vh] bg-[#1A2A33]">
+      <div className="flex mt-[24px] mb-[64px] bg-[#1A2A33] w-[327px] h-[40px] justify-between">
+        <div className="flex flex-row w-[72px] h-[32px] justify-between">
           <img src={xImg} alt="X" />
           <img src={zeroImg} alt="0" />
         </div>
-        <div className=" flex flex-row justify-around items-center w-[96px] h-[40px] rounded-[5px] bg-[#1F3641]">
-          {" "}
-          {/* <img className=" w-[16px] h-[16px]" src={smallX} alt="x" /> */}
+        <div className="flex flex-row justify-around items-center w-[96px] h-[40px] rounded-[5px] bg-[#1F3641]">
           <span>{turn}</span>
-          <span className=" text-[#A8BFC9] text-[14px] font-bold"> TURN</span>
+          <span className="text-[#A8BFC9] text-[14px] font-bold">TURN</span>
         </div>
-        <div className=" flex justify-center items-center bg-[#A8BFC9] w-[40px] h-[40px] rounded-[5px]">
-          {" "}
+        <div className="flex justify-center items-center bg-[#A8BFC9] w-[40px] h-[40px] rounded-[5px]">
           <img src={Rotate} alt="" />
         </div>
       </div>
-      <div className=" flex flex-col  items-center mb-[32px]">
-        <div className=" flex gap-[20px] mb-[20px] ">
+      <div className="flex flex-col items-center mb-[32px]">
+        <div className="flex gap-[20px] mb-[20px]">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
           <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
         </div>
-        <div className=" flex gap-[20px] mb-[20px]">
+        <div className="flex gap-[20px] mb-[20px]">
           <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
           <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
           <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
         </div>
-
-        <div className=" flex gap-[20px]">
+        <div className="flex gap-[20px]">
           <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
           <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
           <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
       </div>
-      <div className=" flex w-[328px] justify-between">
-        {" "}
-        <div className=" flex w-[96px] h-[64px] bg-[#31C3BD] rounded-[10px] justify-center">
-          <span className=" text-[12px]">X (P2)</span>
+      <div className="flex w-[328px] justify-between">
+        <div className="flex w-[96px] h-[64px] bg-[#31C3BD] rounded-[10px] justify-center">
+          <span className="text-[12px]">X (P2)</span>
         </div>
-        <div className=" flex w-[96px] h-[64px] bg-[#A8BFC9] rounded-[10px] justify-center">
-          {" "}
-          <span className=" text-[12px]"> TIES</span>
+        <div className="flex w-[96px] h-[64px] bg-[#A8BFC9] rounded-[10px] justify-center">
+          <span className="text-[12px]">TIES</span>
         </div>
-        <div className=" flex w-[96px] h-[64px] bg-[#F2B137] rounded-[10px] justify-center">
-          <span className=" text-[12px]"> O (P1)</span>
+        <div className="flex w-[96px] h-[64px] bg-[#F2B137] rounded-[10px] justify-center">
+          <span className="text-[12px]">O (P1)</span>
         </div>
       </div>
-      <div> {statusOfWinner ? <span> {statusOfWinner} </span> : null}</div>
+      <div>{winner ? <span>Winner: {winner}</span> : null}</div>
     </div>
   );
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import xImg from "../assets/Combined Shape Copy 2.png";
+import zeroImg from "../assets/Oval Copy.png";
 
 export default function Square({ value, onSquareClick }) {
   return (
     <button
       onClick={onSquareClick}
-      className=" bg-[#1F3641] w-[100px] h-[100px] rounded-[10px]"
+      className=" bg-[#1F3641] w-[100px] h-[100px] rounded-[10px] flex justify-center items-center"
     >
-      {value}
+      {value === "X" && <img src={xImg} alt="X" />}
+      {value === "O" && <img src={zeroImg} alt="O" />}
     </button>
   );
 }
