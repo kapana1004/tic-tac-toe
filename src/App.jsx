a55c2d941b75cdb6740c9ac55821e81821ebcf85
George Kapanadze <52297225+kapana1004@users.noreply.github.com>
2024-02-04 22:37:17 +0400

    complete Entrygame component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Square from "./components/Square";
+import Entrygame from "./components/Entrygame";
 import xImg from "./assets/Combined Shape Copy 2.png";
 import zeroImg from "./assets/Oval Copy.png";
 import smallX from "./assets/SmallX.png";
@@ -13,6 +14,7 @@ function App() {
     <img className="w-[16px] h-[16px]" src={smallX} alt="x" />
   );
   const [winner, setWinner] = useState(null);
+  const [start, setStart] = useState(false);
 
   const handleClick = (i) => {
     if (winner || squares[i]) {
@@ -58,50 +60,59 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col items-center min-h-[100vh] bg-[#1A2A33]">
-      <div className="flex mt-[24px] mb-[64px] bg-[#1A2A33] w-[327px] h-[40px] justify-between">
-        <div className="flex flex-row w-[72px] h-[32px] justify-between">
-          <img src={xImg} alt="X" />
-          <img src={zeroImg} alt="0" />
+    <>
+      <Entrygame start={start} setStart={setStart} />
+      <div
+        className={` ${
+          start
+            ? `flex flex-col items-center min-h-[100vh] bg-[#1A2A33]`
+            : `hidden`
+        } `}
+      >
+        <div className="flex mt-[24px] mb-[64px] bg-[#1A2A33] w-[327px] h-[40px] justify-between">
+          <div className="flex flex-row w-[72px] h-[32px] justify-between">
+            <img src={xImg} alt="X" />
+            <img src={zeroImg} alt="0" />
+          </div>
+          <div className="flex flex-row justify-around items-center w-[96px] h-[40px] rounded-[5px] bg-[#1F3641]">
+            <span>{turn}</span>
+            <span className="text-[#A8BFC9] text-[14px] font-bold">TURN</span>
+          </div>
+          <div className="flex justify-center items-center bg-[#A8BFC9] w-[40px] h-[40px] rounded-[5px]">
+            <img src={Rotate} alt="" />
+          </div>
         </div>
-        <div className="flex flex-row justify-around items-center w-[96px] h-[40px] rounded-[5px] bg-[#1F3641]">
-          <span>{turn}</span>
-          <span className="text-[#A8BFC9] text-[14px] font-bold">TURN</span>
+        <div className="flex flex-col items-center mb-[32px]">
+          <div className="flex gap-[20px] mb-[20px]">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="flex gap-[20px] mb-[20px]">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="flex gap-[20px]">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
         </div>
-        <div className="flex justify-center items-center bg-[#A8BFC9] w-[40px] h-[40px] rounded-[5px]">
-          <img src={Rotate} alt="" />
+        <div className="flex w-[328px] justify-between">
+          <div className="flex w-[96px] h-[64px] bg-[#31C3BD] rounded-[10px] justify-center">
+            <span className="text-[12px]">X (P2)</span>
+          </div>
+          <div className="flex w-[96px] h-[64px] bg-[#A8BFC9] rounded-[10px] justify-center">
+            <span className="text-[12px]">TIES</span>
+          </div>
+          <div className="flex w-[96px] h-[64px] bg-[#F2B137] rounded-[10px] justify-center">
+            <span className="text-[12px]">O (P1)</span>
+          </div>
         </div>
+        <div>{winner ? <span>Winner: {winner}</span> : null}</div>
       </div>
-      <div className="flex flex-col items-center mb-[32px]">
-        <div className="flex gap-[20px] mb-[20px]">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="flex gap-[20px] mb-[20px]">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="flex gap-[20px]">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </div>
-      <div className="flex w-[328px] justify-between">
-        <div className="flex w-[96px] h-[64px] bg-[#31C3BD] rounded-[10px] justify-center">
-          <span className="text-[12px]">X (P2)</span>
-        </div>
-        <div className="flex w-[96px] h-[64px] bg-[#A8BFC9] rounded-[10px] justify-center">
-          <span className="text-[12px]">TIES</span>
-        </div>
-        <div className="flex w-[96px] h-[64px] bg-[#F2B137] rounded-[10px] justify-center">
-          <span className="text-[12px]">O (P1)</span>
-        </div>
-      </div>
-      <div>{winner ? <span>Winner: {winner}</span> : null}</div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Entrygame.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import xImg from "../assets/Combined Shape Copy 2.png";
+import zeroImg from "../assets/Oval Copy.png";
+import smallX from "../assets/SmallX.png";
+import smallZero from "../assets/oval.png";
+
+export default function Entrygame({ start, setStart }) {
+  const handleGameStart = () => {
+    setStart(!start);
+  };
+  return (
+    <div className=" flex flex-col justify-center items-center min-h-[100vh] bg-[#1A2A33] ">
+      <div className=" flex flex-row w-[72px] h-[32px] justify-between mb-[25px]">
+        <img className=" w-[32px] h-[32px]" src={xImg} alt="X" />
+        <img className=" w-[32px] h-[32px]" src={zeroImg} alt="O" />
+      </div>
+
+      <div className=" flex w-[327px] h-[205px]  bg-[#1F3641] rounded-[10px] flex-col items-center">
+        <span className="  text-[#A8BFC9] mt-[20px] ">
+          PICK PLAYER 1’S MARK
+        </span>
+        <div className=" flex w-[279px] h-[72px] mt-[20px] bg-[#1A2A33] rounded-[10px] justify-around items-center ">
+          <div className=" flex justify-center items-center w-[132px] h-[54px] rounded-[5px] hover:bg-[#31C3BD] cursor-pointer ">
+            <img className=" w-[32px] h-[32px]" src={smallX} alt="x" />
+          </div>
+          <div className=" flex justify-center items-center w-[132px] h-[54px] rounded-[5px] hover:bg-[#F2B137] cursor-pointer">
+            <img className=" w-[32px] h-[32px]" src={smallZero} alt="" />
+          </div>
+        </div>
+        <span className=" text-[#A8BFC9] text-[12px] font-light mt-[20px] ">
+          {" "}
+          REMEMBER : X GOES FIRST
+        </span>
+      </div>
+
+      <div className=" flex w-[327px] h-[56px] bg-[#F2B137] hover:bg-[#FFC860] cursor-pointer rounded-[10px] mt-[30px] justify-center items-center">
+        {" "}
+        <span className=" font-bold">NEW GAME (VS CPU)</span>
+      </div>
+      <div
+        onClick={handleGameStart}
+        className=" flex w-[327px] h-[56px] bg-[#31C3BD] hover:bg-[#65E9E4] cursor-pointer rounded-[10px] mt-[17px] justify-center items-center"
+      >
+        {" "}
+        <span className=" font-bold">NEW GAME (VS PLAYER)</span>
+      </div>
+    </div>
+  );
+}
